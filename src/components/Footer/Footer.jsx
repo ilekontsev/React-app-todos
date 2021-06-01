54572d1f87958d3990c0ef58ff62da52d473828c
Ivan <ivan.lek123@gmail.com>
2021-06-01 15:15:09 +0300

    workVers

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,55 @@
+import { PureComponent } from "react";
+import "./Footer.css"
+
+class Footer extends PureComponent{
+
+    render(){
+     
+        return (
+          <div className={this.props.tasks.length ? "footer" : "gone"}>
+            <div className="items">{this.props.items} items left</div>
+            <div className="box">
+              <div
+                className={
+                  this.props.filterValue === "all"
+                    ? "filter activeFilter"
+                    : "filter"
+                }
+                onClick={() => this.props.applyFilter("all")}
+              >
+                All
+              </div>
+              <div
+                className={
+                  this.props.filterValue === "active"
+                    ? "filter activeFilter"
+                    : "filter"
+                }
+                onClick={() => this.props.applyFilter("active")}
+              >
+                Active
+              </div>
+              <div
+                className={
+                  this.props.filterValue === "completed"
+                    ? "filter activeFilter"
+                    : "filter"
+                }
+                onClick={() => this.props.applyFilter("completed")}
+              >
+                Completed
+              </div>
+            </div>
+            <div
+              className= {this.props.items === this.props.tasks.length ? "hidden" : "clear"}
+              onClick={this.props.clearDel}
+
+            >
+              Clear completed
+            </div>
+          </div>
+        );
+    }
+}
+
+export default Footer
